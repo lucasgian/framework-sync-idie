3576b655ea0346b2cbdeba312c102d6849985084
lucasgian <gianextensao@gmail.com>
2018-07-17 21:25:14 -0400

    [feat] adição do método de adição de páginas

--- View/MainView.php
+++ View/MainView.php
@@ -17,23 +17,51 @@ class MainView {
 		require_once __STATIC__.'footer.php'; 
 	}
 
-	public function write() {
+	public function writeApp() {
 		
 		$json = Decode::getJson();
+
 		if (isset($_POST['dir'])) {
 
 			$json->app = $_POST['dir'];	
 			WriteOfFile::setJson("config/project.json", $json);
 			
 		}
-		require_once __STATIC__.'head.php';
-		require_once __STATIC__.'home.php'; 
-		require_once __STATIC__.'footer.php'; 
+		
+		$this->index();
+
 	}
 
-	public function add() {}
-	public function edit() {}
-	public function view() {}
-}
+	public function writeRouter() {
+		
+		$json = Decode::getJson();
+		BootstrapClass::getAllStyle();
+		// $array = (array) $json;
+		//echo key(current($array));
 
-?>
+		BootstrapClass::getCadText( DebugClass::dump_var($json) );
+		die();
+
+		if (isset($_POST['dir']) && isset($_POST['fileName'])) {
+
+			$routes = $json->routes;
+
+			array_push(
+				$routes, array(
+					"name" => "index",
+					"presenter" => $_POST['dir'] . "View", 
+					"page" => $_POST['fileName']
+				)
+			);
+			
+			$json->routes = $routes;
+				
+			WriteOfFile::setJson("database.json", $json);
+			
+		}
+		
+		$this->generation();
+
+	}
+
+}
--- idie/util/WriteOfFile.php
+++ idie/util/WriteOfFile.php
@@ -22,4 +22,3 @@ class WriteOfFile {
 
 }
 
-?>
